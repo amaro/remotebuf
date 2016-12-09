3daa998a80214c808cda0af429c98cb8dfe6d909
Nathan Pemberton <nathanp@berkeley.edu>
2016-12-09 13:53:03 -0800

    OutputStream now uses constant java memory (batches outputs in fixed-sized buffer)

--- ucb/remotebuf/ROutputStream.java
+++ ucb/remotebuf/ROutputStream.java
@@ -3,15 +3,22 @@ package ucb.remotebuf;
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 
 public class ROutputStream extends OutputStream {
-  RemoteBuf.Buffer Buffer;
-  ByteArrayOutputStream LocalBuf;
-  boolean Flushed;
+  private RemoteBuf.Buffer Buffer;
+
+  /* Amount of data to buffer before flushing to the Rmem C++ library */
+  private static final int LocalBufSize = 1024*1024;
+  private ByteBuffer LocalBuf;
+
+  private boolean Flushed;
+  private int absPos; // Absolute position within the block that we are writing
+
 
   public ROutputStream(RemoteBuf.Buffer B) {
     Buffer = B;
-    LocalBuf = new ByteArrayOutputStream();
+    LocalBuf = ByteBuffer.allocate(LocalBufSize);
     Flushed = false;
   }
 
@@ -29,21 +36,36 @@ public class ROutputStream extends OutputStream {
   }
 
   @Override
+  public void write(int b) throws IOException {
+    byte[] bArr = {(byte)b};
+    write(bArr, 0, 1);
+  }
+
+  @Override
   public void write(byte[] b) throws IOException {
     write(b, 0, b.length);
   }
 
   @Override
   public void write(byte[] b, int off, int len) throws IOException {
-    LocalBuf.write(b, off, len);
-  }
 
-  @Override
-  public void write(int b) {
-    LocalBuf.write(b);
+    int rem = LocalBuf.remaining();
+    if(len <= rem) {
+      /* Buffer will fit completely */
+      LocalBuf.put(b, off, len);
+    } else {
+      /* We will run out of room in LocalBuf, trigger a transfer */
+      LocalBuf.put(b, off, rem);
+
+      doActualWrite();
+
+      write(b, off + rem, len - rem);
+    }
   }
 
   private void doActualWrite() {
-    Buffer.write(LocalBuf.toByteArray(), LocalBuf.size());
+    LocalBuf.flip();
+    Buffer.write(LocalBuf, LocalBuf.limit());
+    LocalBuf.clear();
   }
 }
