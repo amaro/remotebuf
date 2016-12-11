e6c0f890dbcb56f8369e2553263d3cde53d70c2b
Sagar Karandikar <sagark@eecs.berkeley.edu>
2016-12-11 13:29:41 -0800

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git submodule update --init
+git submodule update --init --recursive
 pushd javacpp && mvn package && popd
+pushd ddc/third_party/libcuckoo/cityhash-1.1.1 && autoreconf -fis && CXX=g++-6 ./configure && make -j 10 && popd
 pushd ddc && ./bootstrap.sh && make && popd
 sed -i "s#REPLACEME/remotebuf#$(pwd)#g" ucb/remotebuf/config/RemoteBuf.java
 CXXFLAGS=-DRDMA make java
